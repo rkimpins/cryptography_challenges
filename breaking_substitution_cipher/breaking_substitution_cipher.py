4993a62a6b5d44a8d5e50190017240c9cfa586f0
rkimpins <rkimpins@ualberta.ca>
2020-05-07 16:01:36 -0600

    Added documentation for code and cleaned up output formatting

--- breaking_substitution_cipher/breaking_substitution_cipher.py
+++ breaking_substitution_cipher/breaking_substitution_cipher.py
@@ -1,8 +1,42 @@
+"""Substitution cipher solver
+
+This script breaks a message encrypted with a substitution cipher. It uses letter
+and diagraph frequency analysis to complete this. 
+
+Letter frequencies are taken from https://en.wikipedia.org/wiki/Letter_frequency
+Diagraph frequency is taken from http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/digraphs.html
+
+This was inspired by working through the course "Cryptography I" by Stanford 
+University on Coursera. This course can be found at 
+https://www.coursera.org/learn/crypto?, and was available on May 7th, 2020.
+
+This file can also be imported as a module, and contains the following useful function
+    * decrypt_substitution_cipher - returns the decrypted message
+"""
+
 import unidecode
 import collections
 import diagraph_order
+import random
+import string
 
-def clean_message(message):
+def clean_message(message: str) -> str:
+    """Cleans up our message text for use in encryption
+
+    Removes all punctuation, spaces, and converts accented and uppercase
+    letters to lowercase unaccented letters
+
+    Parameters
+    ----------
+    message : str
+        the message that we would like to clean and encrypt
+
+    Returns
+    -------
+    str
+        cleaned up message
+    """
+
     result = ""
     for char in message:
         if char.isalpha():
@@ -11,74 +45,205 @@ def clean_message(message):
     result = unidecode.unidecode(result)
     return result.lower()
 
-def encrypt_substitution_cipher(key,message):
+def encrypt_substitution_cipher(key: dict, message: str) -> str:
+    """Encrypt a message using a simple substitution cipher
+
+    Parameters
+    ----------
+    message : str
+        the message that we would like to encrypt
+
+    Returns
+    -------
+    str
+        encrypted message
+    """
+
     result = ""
     for letter in message:
         result += key[letter]
     return result
 
-def letter_frequency(message):
+def letter_frequency(message: str) -> dict:
+    """Finds the letter frequency from a message
+
+    Parameters
+    ----------
+    message : str
+        the message that we want the letter frequency of
+
+    Returns
+    -------
+    dict
+        (char, frequency) pairs
+    """
+
     return collections.Counter(message)
 
-def decode_e_t_a(encrypted_message):
+def decode_e_t_a(encrypted_message: str) -> list:
+    """Using frequency analysis, solves for e, t, and a in our encrypted message
+
+    Parameters
+    ----------
+    encrypted_message : str
+        the message that has been encrypted with a substitution cipher
+
+    Returns
+    -------
+    list(3)
+        the values of [e, t, a]
+    """
+
     lf = letter_frequency(encrypted_message)
     lf = sorted(lf.items(), key=lambda kv: kv[1])
     lf.reverse()
     return [lf[0][0], lf[1][0], lf[2][0]]
 
-def diagraph_frequency(message):
+def diagraph_frequency(message: str) -> dict:
+    """Finds the diagraph frequency from a message
+
+    An important thing to note is that our message has no spaces, so we are
+    unable to differentiate diagraphs of the last and first letter of orders,
+    which are not counted as diagraphs. Hopefully the frequency of these
+    fake diagraphs are uncommon enough that our analysis will not fail
+
+    Parameters
+    ----------
+    message : str
+        the message that we want the diagraph frequency of
+
+    Returns
+    -------
+    dict
+        (diagraph, frequency) pairs
+    """
+
     count = collections.Counter()
     for i in range(len(message)-1):
         count.update([message[i:i+2]])
     return count
 
+def single_diagraph_search(diagraphs: list, pos: int, letter: chr, nots=[]) -> chr:
+    """Solves for a single letter given its position in a diagraph to find
 
-def single_diagraph_search(diagraphs, pos, letter, nots=[]):
-    print("function args:", pos, letter, nots)
+    Using a diagraph that appears frequently in our encrypted message, and
+    using our knowledge of the expected frequency of diagraphs in english,
+    if one of the letters in the diagraph is known, we can find the other.
+    In this context, find means solving for its substitution
+
+    Parameters
+    ----------
+    diagraphs : list
+        (diagraph, frequency) tuples sorted in descending frequency in our message
+    pos : int
+        position of letter in diagraph
+    letter : chr
+        our choice of letter to solve for
+    nots : list, optional
+        list of letters that our solution can not be. Most likely because they
+        are already found or assumed to be known
+
+    Returns
+    -------
+    chr
+        solution for our letter
+    """
+
     for diagraph in diagraphs:
         if diagraph[0][pos] == letter and diagraph[0][1-pos] not in nots and diagraph[0][0] != diagraph[0][1]:
-            print("diagraph:", diagraph)
             return diagraph[0][1-pos]
     print(f"Failed to find {letter}")
 
-def decrypt_substitution_cipher(encrypted_m):
+def decrypt_substitution_cipher(encrypted_m: str) -> str:
+    """Decodes an encryted message created with a substitution cipher.
+
+    This solution uses letter and diagraph frequency analysis to break a
+    susbtituion cipher.
+
+    Parameters
+    ----------
+    encrypted_m: str
+        the message encrypted by an unknown substitution cipher
+
+    Returns
+    -------
+    (dict, str)
+        dict : the key that was used for the encryption
+        str : the decrypted message
+    """
+
+    # Solve for e,t,a
     key = {}
     [e,t,a] = decode_e_t_a(encrypted_m)
     key["e"] = e
     key["t"] = t
     key["a"] = a
-    print(key)
 
+    # Format diagrapha frequency as a list instead of dict
     diagraphs = diagraph_frequency(encrypted_m)
     diagraphs = sorted(diagraphs.items(), key=lambda kv: kv[1])
     diagraphs.reverse()
 
-    #key["h"] = single_diagraph_search(diagraphs, 0, key["t"]) 
-    print(key)
-    # Manual method I attempted
+    # First attempt at manually ordering diagraph search
     # Using diagraphs th, er, an, in, nd, on, es, ou, ng, of, al, ve, pe, ly, co
     #to_find = ["h","r","n","i","d","o","s","u","g","f","l","v","y","c"]
     #to_use =  ["t","e","a","n","n","n","e","o","n","o","a","e","l","o"]
     #pos =     [ 0 , 0 , 0 , 1 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1 ]
 
-
+    # Use our module to find the best ordering of diagraphs
     (to_find, to_use, pos) = diagraph_order.generate_letter_order(["e","t","a"], ["b","c","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"])
-    # Should return
+    # Should return something like this
     #to_find = ["h","r","s","l","i","n","d","g","v","o","f","u","c","m","w","p","b","y","k","x","q","j"]
     #to_use = ["t","e","t","a","t","i","e","n","e","n","o","o","e","e","a","e","e","l","e","e","u","u"]
     #pos = [ 0 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 1 , 0 , 1 , 1 ]
 
-
-
+    # Run each diagraph solution to solve for all letters
     for i in range(len(pos)):
         key[to_find[i]] = single_diagraph_search(diagraphs, pos[i], key[to_use[i]], key.values())
-        print(key)
 
+    # Find value for last key z
+    for char in string.ascii_lowercase:
+        if char not in key.values():
+            key['z'] = char
+            break
+
+
+    # Reverse our key and decrypt the message
+    reverse_key = {v: k for k, v in key.items()}
+
+    # If a letter is missing for any reason, replace with _
+    for char in string.ascii_lowercase:
+        if char not in reverse_key.keys():
+            reverse_key[char] = "_"
+
+    print(key)
+    decrypted_message = ""
+    for char in encrypted_m:
+        decrypted_message += reverse_key[char]
+
+    return key, decrypted_message
+
+def create_encryption_key() -> dict:
+    """Creates a random substitution key to encrypt the message
+
+    Returns
+    -------
+    dict
+        key value pairs where each is a lowercase alpha value
+    """
+
+    keys = list(string.ascii_lowercase)
+    values = keys.copy()
+    random.shuffle(values)
+    key = {k: v for k,v in zip(keys, values)}
+    return key
 
 def main():
-    #https://en.wikipedia.org/wiki/Letter_frequency
-    #http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/digraphs.html
+    """Encrypts a message loaded from file, then decrypts the message
+    """
+
     filename = "message.txt"
+    key = create_encryption_key()
     key = {"a":"z", "b":"e", "c":"b", "d":"r", "e":"a", "f":"s", "g":"c", 
             "h":"d", "i":"f", "j":"g", "k":"h", "l":"i", "m":"j", "n":"k", 
             "o":"l", "p":"m", "q":"n", "r":"o", "s":"p", "t":"q", "u":"t", 
@@ -87,12 +252,23 @@ def main():
     f = open(filename,"r")
     message = f.read()
     message = clean_message(message)
+
     # Encrypt
     encrypted_message = encrypt_substitution_cipher(key, message)
+
     # Decrypt
-    decrypt_substitution_cipher(message)
+    decryption_key, decrypted_message = decrypt_substitution_cipher(encrypted_message)
+    print("Original message: ", message[0:100])
+    print("Decrypted message:", decrypted_message[0:100])
+
+    print(key, decryption_key)
+    # How accurate were we?
+    total = 0
+    for char in string.ascii_lowercase:
+        if key[char] == decryption_key[char]:
+            total += 1
+    print(f"Algorithm correctly decrypted {total} out of 26 letters")
 
 
-main()
-
-
+if __name__ == "__main__":
+    main()
--- breaking_substitution_cipher/diagraph_order.py
+++ breaking_substitution_cipher/diagraph_order.py
@@ -1,5 +1,26 @@
-# Setup global vaiables that hold diagraph frequency information
+"""Diagraph order generator
+
+This script considers the frequency of different diagrahs in the english
+language and generates the order that we would want to use them to decrypt
+a substitution cipher. A diagraph is a two letter combination that appears
+frequently in the english language, such as "th" 
+
+To choose a good diagrah, we first need a letter that we know the substitution x
+for, and a diagraph that is common enough to notice in some encryted text but
+where the next instance that it could be mistaken for is statistically
+significantly far away. This script attempts to iteratively find the best choice
+for this, until all letters are known.
+
+Diagraph frequencies are taken from 
+http://practicalcryptography.com/media/cryptanalysis/files/english_bigrams_1.txt
+and are stored as a json object (dict(diagraph : frequency)) in diagraph_frequency.txt
+
+This file can also be imported as a module, and contains the following useful function
+    * generate_letter_order - returns three formatted lists detailing diagraphs order
+"""
 import json
+
+# Setup global variables that hold diagraph frequency information
 json_data = open('diagraph_frequency.txt')
 global_diagraph_freq = json.load(json_data)
 
@@ -9,7 +30,28 @@ global_diagraph_freq_list.reverse()
 global_total_diagraphs = sum(global_diagraph_freq.values())
 
 
-def distance_to_next_match(diagraph, letter, pos, unfound):
+
+def distance_to_next_match(diagraph: str, letter: chr, pos: int, unfound: list) -> (int,str):
+    """Given a diagrah and letter, returns how distinct that diagraph is from other letters
+
+    Parameters
+    ----------
+    diagraph : str
+        a string consisting of two lowercase alpha characters
+    letter : chr
+        the letter of interest in the diagraph
+    pos : int
+        position of letter in diagraph
+    unfound : list
+        list of characters that have not yet been found
+
+    Returns
+    -------
+    (int, str)
+        an int representing how good of a choice letter as a percentage difference
+        a string of the best diagraph used
+    """
+
     if diagraph == None:
         return float("-inf"), ""
     min_val = float("inf")
@@ -18,21 +60,41 @@ def distance_to_next_match(diagraph, letter, pos, unfound):
     for c in unfound:
         if c == diagraph[1-pos] or c == letter:
             continue
+        # Consider diagraphs where letter is in pos 0 or 1
         if pos:
             val = c+letter
         else:
             val = letter+c
-        #dias[val] = d[val]
 
-        diff= global_diagraph_freq[diagraph] - global_diagraph_freq[val]
+        diff = global_diagraph_freq[diagraph] - global_diagraph_freq[val]
         if min_val > diff:
             min_val = diff
             min_dia = val
 
-    return min_val/global_total_diagraphs * 100, min_dia
+    return min_val / global_total_diagraphs * 100, min_dia
 
 
-def min_distance_of_letter(letter, found, unfound):
+def min_distance_of_letter(letter: chr, found: list, unfound: list) -> (int,str):
+    """Given a letter, finds the best value for it over all diagraphs
+
+    Parameters
+    ----------
+    letter : chr
+        the letter of interest in the diagraph
+    unfound : list
+        list of characters that have not yet been found
+    found : list
+        list of characters that have been found
+
+    Returns
+    -------
+    (int, str)
+        an int representing how good of a choice a letter is as a percentage 
+        difference of occurences
+        a string of the best diagraph used
+    """
+
+    # Find first matching diagraph for letter, either position
     for item in global_diagraph_freq_list:
         if item[0][0] == letter and item[0][1] in unfound:
             diagraph0 = None
@@ -47,29 +109,66 @@ def min_distance_of_letter(letter, found, unfound):
         if item[0][1] == letter and item[0][0] in found:
             diagraph1 = item[0]
             break
-    #print(diagraph0, diagraph1)
-    (z0,z0d) = distance_to_next_match(diagraph0, letter, 0, unfound)
-    (z1,z1d) = distance_to_next_match(diagraph1, letter, 1, unfound)
-    #print(z0d, z1d)
-    if z0 > z1:
-        return z0, diagraph0
-    else:
-        return z1, diagraph1
 
-def best_choice_of_letter(found, unfound):
+    (dist0, dist1_diagraph) = distance_to_next_match(diagraph0, letter, 0, unfound)
+    (dist1, dist2_diagraph) = distance_to_next_match(diagraph1, letter, 1, unfound)
+    if dist0 > dist1:
+        return dist0, diagraph0
+    else:
+        return dist1, diagraph1
+
+def best_choice_of_letter(found: list, unfound: list) -> (chr, str, int):
+    """Given a list of found and unfound letters, returns the best letter to use
+
+    Parameters
+    ----------
+    found : list
+        list of characters that have been found
+    unfound : list
+        list of characters that have not yet been found
+
+    Returns
+    -------
+    (chr, int, str)
+        a character of the best letter to use
+        a string of the best diagraph used
+        an int representing how good of a choice a letter is as a percentage 
+        difference of occurences
+    """
+
     best_letter = ""
     max_val = float("-inf")
     best_dia = ""
+    # Loop over unfound letters finding the best choice
     for letter in unfound:
         (val,dia) = min_distance_of_letter(letter, found, unfound)
-        #print(letter, val)
         if val > max_val:
             max_val = val
             best_letter = letter
             best_dia = dia
     return best_letter, best_dia, max_val
 
-def generate_letter_order(found, unfound):
+def generate_letter_order(found: list, unfound: list) -> (list, list, list):
+    """Generates the best letter ordering to solve a substitution cipher
+
+    The lists returned are specially formatted for use in a previously written 
+    program. 
+
+    Parameters
+    ----------
+    found : list
+        list of characters that have been found
+    unfound : list
+        list of characters that have not yet been found
+
+    Returns
+    -------
+    (list, list, list)
+        to_find : the unknown letter that we want to solve for in our substitution
+        to_use : the known letter that we will use to solve it
+        pos : the position of our known letter in our diagraph
+    """
+
     result_to_find = []
     result_to_use = []
     result_pos = []
@@ -87,16 +186,26 @@ def generate_letter_order(found, unfound):
     return result_to_find, result_to_use, result_pos
 
 def main():
-    #http://practicalcryptography.com/media/cryptanalysis/files/english_bigrams_1.txt
+    """Very similair fucntionality to generate letter order, but pretty prints output
+
+    This will print our output as
+    letter diagraph value
+    where value is the rating of how good a choice the letter is
+    example:
+        h th 2.24010704830432
+        r re 0.732929984703371
+        s st 0.6535319633072852
+        ...
+        z
+    """
+
+    # Letters assumed to be known and unknown in substitution
     found = ["e","t","a"]
     unfound = ["b","c","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"]
 
-    result_to_find = []
-    result_to_use = []
-    result_pos = []
+    # Loop until all letters are known
     while len(unfound) > 1:
         (letter, dia, val) = best_choice_of_letter(found, unfound)
-
         print(letter, dia, val)
         found.append(letter)
         unfound.remove(letter)
