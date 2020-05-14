ebdc254bd45128dc08bd4236f48ecadbaf946a9a
rkimpins <rkimpins@ualberta.ca>
2020-05-14 11:36:35 -0600

    Debuged string encoding issues, added interactive solver, switched from xoring all cipher combinations to just xoring target and other ciphers

--- breaking_two_time_pad/breaking_two_time_pad.py
+++ breaking_two_time_pad/breaking_two_time_pad.py
@@ -7,10 +7,10 @@ import re
 
 file = open("words.txt", "r")
 GLOBAL_LIST_OF_AMERICAN_ENGLISH_WORDS = [x.strip() for x in file.readlines()]
+file.close()
 
 def xor_strings(a: str, b: str) -> str:
     """Xors two hex strings of different length"""
-
     length = min(len(a), len(b))
     res = ""
     for index in range(length):
@@ -18,39 +18,43 @@ def xor_strings(a: str, b: str) -> str:
     return res
 
 def string_to_hex(x):
-    x = x.encode()
-    return str(binascii.hexlify(x), 'ascii')
+    x = x.encode('ISO-8859-1')
+    return str(binascii.hexlify(x), 'ISO-8859-1')
 
 def hex_to_string(x):
-    return str(binascii.unhexlify(x), 'ascii')
-
+    return str(binascii.unhexlify(x), 'ISO-8859-1')
 
 def crib_drag(crib: str, index: int, xor_messages: str) -> str:
     index *= 2
     return hex_to_string(xor_strings(string_to_hex(crib), xor_messages[index: index+len(crib)*2]))
 
-def crib_drag_all(crib: str, xor_messages: str) -> str:
-    #TODO: not printing out all potential matches for each match
+def crib_drag_all(crib: str, xor_messages: str, cipher_indexes) -> str:
+    index_matches = []
     for index in range(len(xor_messages)//2 - len(crib)):
         res = crib_drag(crib, index, xor_messages)
         matches = match_cribbed_to_words(res)
         if matches:
-            print(f"index: {index}, result: {res}")
             # Shorten match lists that are too long
-            for index in range(len(matches)):
-                if len(matches[index]) > 10:
-                    matches[index] = matches[index][0:10]
-                    matches[index].append("...")
-            print(f"Suggested matches: {matches}")
+            for ind in range(len(matches)):
+                if len(matches[ind]) > 10:
+                    matches[ind] = matches[ind][0:10]
+                    matches[ind].append("...")
+            #print(f"cipher: {cipher_indexes+1}, index: {index}, result: {res}")
+            #TODO consider appending matches aswell
+            index_matches.append((cipher_indexes, index, res))
+    return index_matches
+            #print(f"suggested matches: {matches}")
+            #print(f"decrypt_section(\"{crib}\", {index}, ciphertext_{cipher_indexes[0]}, ciphertext_{cipher_indexes[1]}, target)")
 
-def crib_drag_all_ciphers(crib: str, ciphers):
-    for index1, index2 in itertools.combinations(range(len(ciphers)), 2):
-        print(f"Ciphers {index1+1} and {index2+1}")
-        try:
-            xor_messages = xor_strings(ciphers[index1], ciphers[index2])
-            crib_drag_all(crib, xor_messages)
-        except:
-            print("failed")
+def crib_drag_all_ciphers(crib: str, ciphers, target):
+    final_result = []
+    for index in range(len(ciphers)):
+    #for index1, index2 in itertools.combinations(range(len(ciphers)), 2):
+        xor_messages = xor_strings(ciphers[index], target)
+        #TODO matches is poorly named. Fix this
+        matches = crib_drag_all(crib, xor_messages, index)
+        final_result += matches
+    return final_result
 
 def remove_punctuation(string):
     for punctuation in ['.', ',', '?', '!']:
@@ -63,8 +67,9 @@ def contains_punctuation(string):
             return True
     return False
 
+
 def regex_is_part_of_word(string):
-    p = re.compile(string)
+    p = re.compile(string, re.IGNORECASE)
     matches = []
     for word in GLOBAL_LIST_OF_AMERICAN_ENGLISH_WORDS:
         m = p.match(word)
@@ -73,6 +78,7 @@ def regex_is_part_of_word(string):
     return matches
 
 def convert_cribbed_to_regex(crib_res):
+    #TODO what happens when we have multiple spaces?
     crib_split = crib_res.split(" ")
     if len(crib_split) == 1:
         crib_split[0] = ".*" + crib_split[0] + ".*"
@@ -95,17 +101,26 @@ def convert_cribbed_to_regex(crib_res):
     crib_split = list(filter(lambda x: x != "", crib_split))
     return crib_split
 
+def contains_illegal_symbols(string):
+    for char in string:
+        #if not str.isalpha(char) and not char.isdigit() and char not in [" ",",",".","!","?","'",'"']:
+        if not str.isalpha(char) and char not in [" "]:
+            return True
+    return False
+
 def match_cribbed_to_words(crib_res):
     total = 0
     multi_word_matches = []
 
     #TODO make this play nice with normal punctuation .,!?
     #darn regex
-    if not str.isalpha(crib_res):
+    if contains_illegal_symbols(crib_res):
+        #print(f"Illegal: {crib_res}")
         return []
+    #if not str.isalpha(crib_res):
+        #return []
 
     crib_split = convert_cribbed_to_regex(crib_res)
-    print(crib_split)
 
     # Check that each partial is a word
     for partial in crib_split:
@@ -116,6 +131,43 @@ def match_cribbed_to_words(crib_res):
         else:
             return False
     return multi_word_matches
+
+def decrypt_section(crib, index, ciphertext1, ciphertext2, target):
+    key1 = xor_strings(string_to_hex(crib), ciphertext1[index:])
+    key2 = xor_strings(string_to_hex(crib), ciphertext2[index:])
+    result1 = hex_to_string(xor_strings(target[index:], key1))
+    result2 = hex_to_string(xor_strings(target[index:], key2))
+    print(result1)
+    print(result2)
+
+def interactive_solver(ciphers, target):
+    solution = ["_" for x in range(len(target)//2)]
+    while(True):
+        print(solution)
+        crib = input("Enter a crib (:q to quit):")
+        if crib == ":q":
+            break
+        matches = crib_drag_all_ciphers(crib, ciphers, target)
+        print("(Cipher, Index, Result)")
+        for pos, match in enumerate(matches):
+            print(f"[{pos}] {match}")
+
+        if len(matches) == 0:
+            continue
+
+        index = input("Choose a matching index, or [None]:")
+        if index != "None":
+            index = int(index)
+            ans = input("Is this solution with the target or the cipher [target, cipher]:")
+            if ans == "target":
+                for pos, char in enumerate(crib):
+                    solution[matches[index][1] + pos] = char
+            elif ans == "cipher":
+                for pos, char in enumerate(matches[index][2]):
+                    solution[matches[index][1] + pos] = char
+            else:
+                print("Invalid input")
+
 
 
 def main():
@@ -129,9 +181,7 @@ def main():
         most_common_words = [ " the ", " and ", " that ", " have ", " for ", " not ", " with ", " you ", " this ", " but ", " his ", " by ", " from ", " they ", " say ", " she ", " will ", " one ", " all ", " would ", " there ", " their ", " what ", " out ", " about ", " who ", " get ", " which ", " when ", " make ", " can ", " like ", " time ", " just ", " him ", " know ", " take ", " people ", " into ", " year ", " your ", " good ", " some ", " could ", " them ", " see ", " other ", " than ", " then ", " now ", " look ", " only ", " come ", " its ", " over ", " think ", " also ", " back ", " after ", " use ", " two ", " how ", " our ", " work ", " first ", " well ", " way ", " even ", " new ", " want ", " because ", " any ", " these ", " give ", " day ", " most ", " her "]
     '''
 
-    print(match_cribbed_to_words("robab"))
-
-
+    #print(match_cribbed_to_words("robab"))
 
     ciphertext_1 = "315c4eeaa8b5f8aaf9174145bf43e1784b8fa00dc71d885a804e5ee9fa40b16349c146fb778cdf2d3aff021dfff5b403b510d0d0455468aeb98622b137dae857553ccd8883a7bc37520e06e515d22c954eba5025b8cc57ee59418ce7dc6bc41556bdb36bbca3e8774301fbcaa3b83b220809560987815f65286764703de0f3d524400a19b159610b11ef3e"
     ciphertext_2 = "234c02ecbbfbafa3ed18510abd11fa724fcda2018a1a8342cf064bbde548b12b07df44ba7191d9606ef4081ffde5ad46a5069d9f7f543bedb9c861bf29c7e205132eda9382b0bc2c5c4b45f919cf3a9f1cb74151f6d551f4480c82b2cb24cc5b028aa76eb7b4ab24171ab3cdadb8356f"
@@ -147,10 +197,48 @@ def main():
     ciphertexts = [ciphertext_1, ciphertext_2, ciphertext_3, ciphertext_4, ciphertext_5,
         ciphertext_6, ciphertext_7, ciphertext_8, ciphertext_9, ciphertext_10]
 
-    crib_drag_all_ciphers(" the ", ciphertexts)
-    #xor_messages = xor_strings(ciphertext_1, ciphertext_10)
-    #print(xor_messages)
-    #crib_drag_all(" the ", xor_messages)
+    #crib_drag_all_ciphers(" the ", ciphertexts, target)
+    #[0] (0, 13, 'ssage')
+    #[1] (4, 60, 'alize')
+    #[2] (5, 60, 'ecret')
+    #[3] (6, 51, 'never')
+    #[4] (6, 60, 'rnmen')
+
+    #x = crib_drag("message", 11, xor_strings(ciphertext_1, target))
+    #print(x)
+    #matches = match_cribbed_to_words(x)
+    #print(matches)
+
+    #print(contains_illegal_symbols("bhba"))
+    interactive_solver(ciphertexts, target)
+
+
+
+    '''
+    for a result of crib drag in cipher x and y
+    for all x combinations, check that they match at that index
+    for all y combinations, check that they match at that index
+    if one of these is signifigant (like > 3, b/c my matcher is weak)
+    Try decrypting the target and all the other strings using this thing
+    '''
+    '''
+    Input a crib drag
+    It returns possible sections that it could have come from
+    '''
+    '''
+    Make this interactive!!
+    Input a crib
+    Walks through every xored cipher, trying to find a matching index
+    manually pick one
+    Prints out all 10 + target after decrypting
+    loop
+    '''
+
+
+
+
+    #TODO add hints for common words
+
 
 if __name__ == "__main__":
     main()
